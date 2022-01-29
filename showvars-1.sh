cf81d210d3e29ff127682eb5dd4c6f962514ba1d
Damian Wojciech Maza <308127@ameryk.fizyka.umk.pl>
2022-01-29 15:33:24 +0100

    first commit

---
+++ showvars-1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "USER=$USER"
+echo "HOME=$HOME"
+echo "LANG=$LANG"
+echo "HISTSIZE=$HISTSIZE"
---
+++ x.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+## utworzenie pliku i nadanie mu praw
+
+touch $1
+
+chmod a+x $1
+
+echo "Utworzono"
