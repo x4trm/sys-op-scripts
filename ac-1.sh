0843e0bef8c4d8def034ba1e5709f3d59dd31474
Damian Wojciech Maza <308127@ameryk.fizyka.umk.pl>
2022-01-31 01:03:25 +0100

    ac-1 update

--- ac-1.sh
+++ ac-1.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-ac -p | sort -k 2 -nr | head -2 | tail -1 | gawk {'print $0'}
+echo `ac -p | sort -k 2 -nr | head -2 | tail -1 | gawk {'print $0'}`
+#ac -p | sort -k 2 -nr | head -2 | tail -1 | gawk {'print $0'}
